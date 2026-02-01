a8dd4cd43613db047851f9e10b98ccb58218028d
Troy Fam <troyfam@Mac-mini.localdomain>
2026-01-31 18:11:08 -0800

    Allow mac-mini host in development

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -3,6 +3,10 @@ require "active_support/core_ext/integer/time"
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
+  # Allow access from local network
+  config.hosts << "mac-mini"
+  config.hosts << ".local"
+
   # Make code changes take effect immediately without server restart.
   config.enable_reloading = true
 
